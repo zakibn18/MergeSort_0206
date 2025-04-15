0fcfb3495041f8b6d9fae722b75c8e906204921c
zakibn18 <jekben815@gmail.com>
2025-04-15 11:42:43 +0700

    membuat input panjang element array

--- mergesort.cpp
+++ mergesort.cpp
@@ -6,7 +6,20 @@ int n;
 
 void input()
 {
-
+  while (true)
+  {
+    cout << "Masukkan panjang element array = ";
+    cin >> n;
+    if (n <= 20)
+    {
+      break;
+    }
+    else
+    {
+      cout << "\nMaksimal panjang array adalah 20";
+    }
+  }
+  
 }
 
 int main() {
