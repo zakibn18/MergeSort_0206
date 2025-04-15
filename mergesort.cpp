f94d8ef7954b8fa9bf65ae03384227e81bab30af
zakibn18 <jekben815@gmail.com>
2025-04-15 12:40:42 +0700

    membuat proces mergeSort

--- mergesort.cpp
+++ mergesort.cpp
@@ -32,7 +32,52 @@ void input()
 
 void mergeSort(int low, int high)
 {
+  if (low >= high)
+  {
+    return;
+  }
+
+  int mid = (low + high) / 2;
+
+  mergeSort(low, mid);
+  mergeSort(mid + 1, high);
+
+  int i = low;
+  int j = mid + 1;
+  int k = low;
+
+  while (i <= mid && j <= high)
+  {
+    if (arr[i] <= arr[j])
+    {
+      B[k] = arr[i];
+      i++;
+    }
+    else
+    {
+      B[k] = arr[j];
+      j++;
+    }
+    k++;
+  }
   
+  while (j <= high)
+  {
+    B[k] = arr[j];
+    j++;
+    k = k + 1;
+  }
+
+  while (i <= mid)
+  {
+    B[k] = arr[i];
+    i++;
+    k++;
+  }
+   for (int x = low; x <= high; x++)
+   {
+    arr[x] = B[x];
+   }
 }
 
 int main() {
